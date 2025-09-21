607425bc9215c8e86efed859f15bf9a5ecf347fd
punnfect <sp.kool@yahoo.com>
2025-09-20 22:59:45 -0500

    Initial Commit

---
+++ src/test/java/com/github/punnfect/workout_tracker/WorkoutTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.punnfect.workout_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
