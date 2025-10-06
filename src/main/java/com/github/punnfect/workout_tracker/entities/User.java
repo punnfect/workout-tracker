1f57bf4c35ad46a1669664ddca9c477b3b6509e4
punnfect <sp.kool@yahoo.com>
2025-10-06 17:09:28 -0500

    Adds columns to users/workout

--- src/main/java/com/github/punnfect/workout_tracker/entities/User.java
+++ src/main/java/com/github/punnfect/workout_tracker/entities/User.java
@@ -1,4 +1,37 @@
 package com.github.punnfect.workout_tracker.entities;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String username;
+
+    @Column(name = "password_hash", nullable = false)
+    private String password;
+
+    @Column(name = "created_at", updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Workout> workouts;
 }
--- src/main/java/com/github/punnfect/workout_tracker/entities/Workout.java
+++ src/main/java/com/github/punnfect/workout_tracker/entities/Workout.java
@@ -1,4 +1,40 @@
 package com.github.punnfect.workout_tracker.entities;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "workouts")
 public class Workout {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "workout_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name ="workout_date", nullable = false)
+    private LocalDate workoutDate;
+
+    @Column(name = "time_enter")
+    private LocalTime timeEnter;
+
+    @Column(name = "time_leave")
+    private LocalTime timeLeave;
+
+    @Column(name = "notes", columnDefinition = "TEXT")
+    private String notes;
+
 }
