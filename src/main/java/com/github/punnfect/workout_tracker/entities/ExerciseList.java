f3779f24c567c32358899ebe65dc453ffdb5dcf7
punnfect <sp.kool@yahoo.com>
2025-10-06 17:32:32 -0500

    Adds columns to ExerciseList/Set.

--- src/main/java/com/github/punnfect/workout_tracker/entities/ExerciseList.java
+++ src/main/java/com/github/punnfect/workout_tracker/entities/ExerciseList.java
@@ -1,4 +1,31 @@
 package com.github.punnfect.workout_tracker.entities;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "exercise_list")
 public class ExerciseList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "exercise_list_id")
+    private Long id;
+
+    @Column(name = "name", nullable = false,unique = true, length = 100)
+    private  String name;
+
+    @Column(name = "category", length = 50)
+    private String category;
+
+    @OneToMany(mappedBy = "exerciseList", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ExerciseSet> exerciseSets;
 }
--- src/main/java/com/github/punnfect/workout_tracker/entities/ExerciseSet.java
+++ src/main/java/com/github/punnfect/workout_tracker/entities/ExerciseSet.java
@@ -1,4 +1,43 @@
 package com.github.punnfect.workout_tracker.entities;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "exercise_sets", uniqueConstraints = {@UniqueConstraint(columnNames = {"workout_id", "exercise_list_id", "set_number"})})
 public class ExerciseSet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "set_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "workout_id", nullable = false)
+    private Workout workout;
+
+    @ManyToOne
+    @JoinColumn(name = "exercise_list_id", nullable = false)
+    private ExerciseList exerciseList;
+
+    @Column(name = "set_number", nullable = false)
+    private Integer setNumber;
+
+    @Column(name = "weight", precision = 7, scale = 2)
+    private BigDecimal weight;
+
+    @Column(name = "reps")
+    private Integer reps;
+
+    @Column(name = "notes", length = 255)
+    private String notes;
+
 }
