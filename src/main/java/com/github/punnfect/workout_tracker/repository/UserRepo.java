0f1af6258f6deccf86fb7adb78e513d2d2222c13
punnfect <sp.kool@yahoo.com>
2025-10-07 16:16:25 -0500

    Adds UserRepo and WorkoutRepo functions to use in services.

--- src/main/java/com/github/punnfect/workout_tracker/repository/UserRepo.java
+++ src/main/java/com/github/punnfect/workout_tracker/repository/UserRepo.java
@@ -4,6 +4,11 @@ import com.github.punnfect.workout_tracker.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepo extends JpaRepository<User, Long> {
+
+    //Finds user by username, used for connecting a user to almost everything
+    Optional<User> findByUsername(String username);
 }
--- src/main/java/com/github/punnfect/workout_tracker/repository/WorkoutRepo.java
+++ src/main/java/com/github/punnfect/workout_tracker/repository/WorkoutRepo.java
@@ -1,9 +1,16 @@
 package com.github.punnfect.workout_tracker.repository;
 
+import com.github.punnfect.workout_tracker.entities.User;
 import com.github.punnfect.workout_tracker.entities.Workout;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WorkoutRepo extends JpaRepository<Workout, Long> {
+
+    //Will list a users workouts from new to old
+    List<Workout> findByUserOrderByWorkoutDateDesc(User user);
+
 }
