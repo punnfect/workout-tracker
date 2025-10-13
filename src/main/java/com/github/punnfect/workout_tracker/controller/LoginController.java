01b549423dfc2bd71f4b9d6de3401483b10a110d
punnfect <sp.kool@yahoo.com>
2025-10-13 16:55:30 -0500

    Creates LoginController and WorkoutController base classes.

---
+++ src/main/java/com/github/punnfect/workout_tracker/controller/LoginController.java
@@ -0,0 +1,7 @@
+package com.github.punnfect.workout_tracker.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginController {
+}
---
+++ src/main/java/com/github/punnfect/workout_tracker/controller/WorkoutController.java
@@ -0,0 +1,8 @@
+package com.github.punnfect.workout_tracker.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WorkoutController {
+
+}
